65adeedbd4376dc1d40f0dade9b18fe8a1f3509c
Von Christian Halip <vc.halip@gmail.com>
2017-06-03 16:51:53 +0800

    Add test for failing account creation

--- spec/features/accounting/accounts/create_account_feature_spec.rb
+++ spec/features/accounting/accounts/create_account_feature_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 feature "Create account" do
-  scenario "with valid attributes" do
+
+  before(:each) do
     visit accounting_accounts_path
     click_link "New Account"
+  end
+
+  scenario "with valid attributes" do
     fill_in "Name", with: "Cash on Hand"
     fill_in "Account code", with: "009090GH"
     fill_in "Type", with: "Accounting::Asset"
     click_button "Create Account"
-    
+
     expect(page).to have_content("created successfully")
   end
+  
+  scenario 'with invalid attributes' do
+    click_button "Create Account"
+
+    expect(page).to have_content("can't be blank")
+  end
+
 end
